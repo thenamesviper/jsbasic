be42b077ffc310a004dbaee0fdc8748c055dd60e
James F Odeh <thenamesviper@gmail.com>
2015-02-18 19:17:13 -0800

    First working build

--- calculator.js
+++ calculator.js
@@ -1,6 +1,39 @@
 var Calculator = {
+		selectOperation: function(operator, firstNumber, secondNumber) {
+			switch(operator) {
+				case "+":
+					return this.add(firstNumber, secondNumber);
+					break;
+				case "-":
+					return this.subtract(firstNumber,secondNumber);
+					break;
+				case "X":
+					return this.multiply(firstNumber, secondNumber);
+					break;
+				case "\u00F7": //division sign
+					return this.divide(firstNumber, secondNumber);
+					break;
+				case "^":
+					return this.toThePower(firstNumber, secondNumber);
+					break;
+				default:
+					return "ERROR";
+			}
+		},
+		immediateOperation: function(operator, number) {
+			switch(operator) {
+				case "x\u00B2": //x^2
+					return this.square(number);
+					break;
+				case "\u221A": //radical symbol
+					return this.squareRoot(number);
+					break;
+				default:
+					return "ERROR";
+			}
+		},
 		add: function(x,y) {
-			return x + y;
+			return x+y;
 		},
 		subtract: function(x,y) {
 			return x-y;
@@ -23,25 +56,85 @@ var Calculator = {
 	};
 	
 $(document).ready(function() {
+	var $firstNumber;
+	var $secondNumber;
+	var $operatorSelected = "";
+	var operatorChosen = false;
+	
+	function displayResults($result) {
+		$resultLength = $result.toString().length;
+		if($resultLength<28 && isFinite($result)) {
+			if($resultLength<12) {
+				$("#output").css("font-size", "87px");	
+			} else if($resultLength<22) {
+				$("#output").css("font-size", "45px");
+			} else {
+				$("#output").css("font-size", "35px");
+			};
+			$("#output").text($result);
+		} else {
+			$("#output").addClass("too-large");
+			$("#output").text("Number is too large");
+		}
+		
+	};
+	
 	$("#equals").click(function() {
-		alert(Calculator.add(3,4));
+		$secondNumber = parseFloat($("#output").text());
+		$value = Calculator.selectOperation($operatorSelected, $firstNumber, $secondNumber);
+		displayResults($value);
+		$operatorSelected = "";
 	}),
 	
 	//view inputted numbers on screen
 	$(".digit").click(function() { 
 		var $outputText = $("#output").text();
-		var containsDecimal = false;
-		if ($outputText.length<11) {
-			if($outputText == "0") {
-				$("#output").text("");
-				$outputText = "";
-			}
-			$("#output").text($outputText + $(this).text());
+		if($outputText == "0" || operatorChosen) {			//prevents leading zeroes
+			displayResults("");
+			$outputText = "";
+			$(".operator").removeClass("operator-selected");
+			operatorChosen = false;
 		}
+		var $thisText = $(this).text();
+		if($(this).text() == "\u00B7") { 			//check to see if middle dot selected
+			if($outputText.indexOf(".") === -1) {   //allows only one decimal
+				displayResults($outputText + ".");
+				}
+		} else {
+				displayResults($outputText + $thisText);
+		}
+	}),
+	
+	$(".operator").click(function() { //operators that take two numbers
+		if (!operatorChosen) {
+			$firstNumber = parseFloat($("#output").text());
+		};
+		if($operatorSelected == $(this).text()) {  //clicking on operation again cancels it
+			$operatorSelected = "";
+			$(this).removeClass("operator-selected");
+		} else {
+			$(".operator").removeClass("operator-selected");
+			$operatorSelected = $(this).text();
+			$(this).addClass("operator-selected");
+			operatorChosen = true;
+		};
+	}),
+	$(".immediate-operator").click(function() { //operators that take only one number
+		var $number = parseFloat($("#output").text());
+		var $operator = $(this).text();
+		$value = Calculator.immediateOperation($operator, $number);
+		displayResults($value);
+	}),
+	
+	$("#pi").click(function() {
+		displayResults("3.14159265359");
 	}),
 
 	$("#clear").click(function() {
-		$("#output").text("0");
+		displayResults("0");
+		operatorChosen = false;
+		$(".operator").removeClass("operator-selected");
+		$("#output").removeClass("too-large");
 	})
 });
 
---
+++ readme.md
@@ -0,0 +1,10 @@
+Basic calculator
+
+TODO:
+
+*No need for both operatorChosen & $operatorSelected
+*Change behavior when = hit before ready
+*General refactoring
+*Allow "Number too large" to be erased when choosing new number?
+*Add support for keyboard input?
+*Add other functions?
